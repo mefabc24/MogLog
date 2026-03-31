b748d0a946d81dae64ce938aefa6046953a49151
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-03-31 19:35:10 +0200

    Refactor MuscleTarget to use Muscle enum instead of MuscleGroup

--- app/src/main/java/com/fabi/moglog/model/MuscleTarget.kt
+++ app/src/main/java/com/fabi/moglog/model/MuscleTarget.kt
@@ -1,9 +1,9 @@
 package com.fabi.moglog.model
 
 import com.fabi.moglog.model.enums.EffectivenessLevel
-import com.fabi.moglog.model.enums.MuscleGroup
+import com.fabi.moglog.model.enums.Muscle
 
 data class MuscleTarget(
-    val MuscleGroup: MuscleGroup,
+    val muscle: Muscle,
     val effectivenessLevel: EffectivenessLevel
 )
