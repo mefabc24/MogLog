f61b4fca4327cfab7bc42eed47e8dac22fa51159
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-04-01 18:05:12 +0200

    Add TrainingProfileRepository for CRUD operations

---
+++ app/src/main/java/com/leet/moglog/fitness/TrainingProfileRepository.kt
@@ -0,0 +1,21 @@
+package com.leet.moglog.fitness
+
+class TrainingProfileRepository(val dao: TrainingProfileDao) {
+
+    // CRUD operations for TrainingProfile
+    suspend fun createProfile(profile: TrainingProfile) {
+        dao.insert(profile)
+    }
+
+    suspend fun getAllProfiles(): List<TrainingProfile> {
+        return dao.getAll()
+    }
+
+    suspend fun updateProfile(profile: TrainingProfile) {
+        dao.update(profile)
+    }
+
+    suspend fun deleteProfile(profile: TrainingProfile) {
+        dao.delete(profile)
+    }
+}
