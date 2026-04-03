4b0370c1e2b73fdc151572dd0897750b79665a3a
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-04-03 12:30:29 +0200

    Enhance WorkoutSplit enum to include parts count for each split type and add new split

--- app/src/main/java/com/leet/moglog/workout/enums/WorkoutSplit.kt
+++ app/src/main/java/com/leet/moglog/workout/enums/WorkoutSplit.kt
@@ -1,9 +1,10 @@
 package com.leet.moglog.workout.enums
 
-enum class WorkoutSplit {
-    FULL_BODY,
-    UPPER_LOWER,
-    PUSH_PULL_LEGS,
-    BRO_SPLIT,
-    TORSO_LIMB_SPLIT
+enum class WorkoutSplit(val parts: Int) {
+    FULL_BODY(1),
+    UPPER_LOWER(2),
+    TORSO_LIMB_SPLIT(2),
+    PUSH_PULL_LEGS(3),
+    ARMS_TORSO_LEGS(3),
+    BRO_SPLIT(5)
 }
