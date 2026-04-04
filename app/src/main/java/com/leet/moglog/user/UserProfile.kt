c8865e4f644a26c78913cbc4de33cbe5bb0da141
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-04-04 18:10:14 +0200

    Enhance UserProfile entity with foreign key relationships and add UserProfileWithRelations data class for improved data handling

--- app/src/main/java/com/leet/moglog/user/UserProfile.kt
+++ app/src/main/java/com/leet/moglog/user/UserProfile.kt
@@ -1,12 +1,35 @@
 package com.leet.moglog.user
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.leet.moglog.profile.TrainingProfile
 import com.leet.moglog.user.enums.Gender
 import java.time.LocalDate
 import java.util.UUID
 
-@Entity(tableName = "user_profiles")
+@Entity(
+    tableName = "user_profiles",
+    foreignKeys  = [
+        ForeignKey(
+            entity = TrainingProfile::class,
+            parentColumns = ["id"],
+            childColumns = ["activeTrainingProfileId"],
+            onDelete = ForeignKey.SET_NULL
+        ),
+        ForeignKey(
+            entity = UserPreferences::class,
+            parentColumns = ["id"],
+            childColumns = ["userPreferencesId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ],
+    indices = [
+        Index("activeTrainingProfileId"),
+        Index("userPreferencesId")
+    ]
+)
 data class UserProfile(
     @PrimaryKey val userId: UUID = UUID.randomUUID(),
     val displayName: String,
